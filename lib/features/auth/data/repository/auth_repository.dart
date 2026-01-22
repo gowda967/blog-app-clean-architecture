847969035efa6c2b768660542441536d2141e95b
Ravitheja v <ravitheja.v@fedo.ai>
2026-01-22 22:00:34 +0530

    postgres setup in supabase and signup completed

--- lib/features/auth/data/repository/auth_repository.dart
+++ lib/features/auth/data/repository/auth_repository.dart
@@ -1,6 +1,7 @@
 import 'package:blog_app/core/error/exceptions.dart';
 import 'package:blog_app/core/error/failures.dart';
 import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/domain/entities/user.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
 import 'package:fpdart/src/either.dart';
 
@@ -9,25 +10,25 @@ class AuthRepositoryImpl implements AuthRepository {
   const AuthRepositoryImpl(this.authRemoteDataSource);
 
   @override
-  Future<Either<Failure, String>> signupWithEmailPassword({
+  Future<Either<Failure, User>> signupWithEmailPassword({
     required String name,
     required String email,
     required String password,
   }) async {
     try {
-      final userId = await authRemoteDataSource.signupWithEmailPassword(
+      final user = await authRemoteDataSource.signupWithEmailPassword(
         name: name,
         email: email,
         password: password,
       );
-      return right(userId);
+      return right(user);
     } on ServerException catch (e) {
       return left(Failure(e.message));
     }
   }
 
   @override
-  Future<Either<Failure, String>> loginWithEmailPassword({
+  Future<Either<Failure, User>> loginWithEmailPassword({
     required String email,
     required String password,
   }) {
--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/features/auth/domain/entities/user.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_signup.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -10,6 +11,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     : _userSignUp = userSignUP,
       super(AuthInitial()) {
     on<AuthSignUp>((event, emit) async {
+      emit(AuthLoading());
       final res = await _userSignUp(
         UserSignUpParams(
           name: event.name,
@@ -19,7 +21,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       );
       res.fold(
         (l) => emit(AuthFailure(l.message)),
-        (r) => emit(AuthSuccess(r)),
+        (user) => emit(AuthSuccess(user)),
       );
     });
   }
